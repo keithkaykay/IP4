7fe1a737bd1939a89b6d704fab31208525f7c911
keithkaykay <kaykaykeith@yahoo.com>
2021-06-27 23:38:32 +0300

    add javascript for getting price for pizza

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function getSize() {
+  var size = document.getElementById("size").value;
+  return parseInt(size);
+}
+
+function getCrust() {
+  var crust = document.getElementById("crust").value;
+  return parseInt(crust);
+}
+
+function getTopping() {
+  var topping = document.getElementById("topping").value;
+  return parseInt(topping);
+}
+
+function getQuantity() {
+  var number = document.getElementById("number").value;
+  return parseInt(number);
+}
